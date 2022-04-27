3e778b51dac3f9e6385a8e9b15409d9603ba54cb
4ist <fpailes@DESKTOP-T0Q4PSP.localdomain>
2022-04-27 13:42:58 -0600

    Created attraction and functionality for adding new attractions

--- destination.py
+++ destination.py
@@ -1,6 +1,9 @@
 
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "SaoPaulo, Brazil", "Cairo, Egypt"]
 
+attractions = [[] for elements in destinations]
+
+
 test_traveler = ['Erin Wilkes', 'Shanghai, China', 'historical site', 'art',]
 
 def get_destination_index(destination):
@@ -15,8 +18,24 @@ def get_traveler_location(traveler):
 
     return traveler_destination_index
 
+def add_attraction(destination, attraction):
+#    attractions[get_destinations_index(destination)] = attractions[get_destinations_index(destination)].append(attraction)
+    destination_index = get_destination_index(destination)
+    attractions_for_destination = attractions[destination_index]
+    attractions_for_destination.append(attraction)
+    return attractions_for_destination
+
+
+add_attraction("Paris, France", ["the Louvre", ['art', 'museam']])
+add_attraction("Paris, France", ["Arc de triomphe", ["historical site", "monumnet"]])
+add_attraction("Shanghai, China"["yu Garden",["garden", "historical site"]])
+add_attraction("Shanghai, China"["Yuz Museam", ["art", "museam"]])
+
+
 
 print(get_destination_index("Los Angeles, USA"))
 print(get_traveler_location(test_traveler))
+add_attraction("Los Angeles, USA", ['Venice Beach', ['Beach']])
+print(attractions)
 
 
