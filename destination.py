0b2ac9ff96c4008f3208bcfe8bed17a9b2132080
4ist <fpailes@DESKTOP-T0Q4PSP.localdomain>
2022-04-27 11:06:15 -0600

    Added test objects

---
+++ destination.py
@@ -0,0 +1,5 @@
+
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "SaoPaulo, Brazil", "Cairo, Egypt"]
+
+test_traveler = ['Erin Wilkes', 'Shanghai, China', 'historical site', 'art',]
+
